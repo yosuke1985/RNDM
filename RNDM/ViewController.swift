fba90478953de4e52b34058672879080e5b0768b
YOSUKE NAKAYAMA <ihatenonoildressing@gmail.com>
2019-05-19 10:49:36 +0900

    Initial Commit

---
+++ RNDM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RNDM
+//
+//  Created by YOSUKE on 2019/05/19.
+//  Copyright © 2019 YOSUKE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
