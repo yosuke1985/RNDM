dbf7484180eb36d1686ae67388ea094ce043c638
YOSUKE NAKAYAMA <ihatenonoildressing@gmail.com>
2019-05-20 00:03:08 +0900

    UI完成

--- RNDM/Controller/AddThoughtVC.swift
+++ RNDM/Controller/AddThoughtVC.swift
@@ -8,23 +8,31 @@
 
 import UIKit
 
-class AddThoughtVC: UIViewController {
+class AddThoughtVC: UIViewController, UITextViewDelegate {
 
+    @IBOutlet weak var segmentCategory:UISegmentedControl!
+    @IBOutlet weak var userNameText: UITextField!
+    @IBOutlet weak var thoughtText: UITextView!
+    @IBOutlet weak var postBtn:UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        postBtn.layer.cornerRadius = 4
+        thoughtText.layer.cornerRadius = 4
+        thoughtText.text = "My random thought..."
+        thoughtText.textColor = UIColor.lightGray
+        thoughtText.delegate = self
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func postBtnTapped(_ sender: Any) {
+        
     }
-    */
-
+    
+    @IBAction func categoryChanged(_ sender: Any) {
+    }
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textView.text = ""
+        textView.textColor = UIColor.lightGray
+    }
 }
